9a6fae9d388737d15e9bcc46c5332f2a6cc78bdd
melo04 <kohhmelody@gmail.com>
2023-04-11 18:31:58 +0800

    new commit

--- components/TypeWriterText2.js
+++ components/TypeWriterText2.js
@@ -3,7 +3,7 @@ import Typewriter from "typewriter-effect";
 export default function TypeWriterText2() {
   return (
     <>
-      <div className="mx-4 md:mx-40 px-4 md:px-8 py-8 bg-white shadow-2xl rounded-lg">
+      <div className="mx-4 mb-20 md:mx-40 px-4 md:px-8 py-8 bg-white shadow-2xl rounded-lg">
         <h1 className="text-xl md:text-3xl mx-4 md:mx-40 capitalize font-extrabold text-black self-center text-center font-nunito">
           Unleash Your{" "}
           <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-800 via-blue-400 to-green-500">
